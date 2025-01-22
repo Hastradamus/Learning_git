11925d2ccee00bf18d026abd2612995ea9ca9daf
Hadiyev <HHadiyev@cwlp.com>
2025-01-22 15:01:49 -0600

    =First Commit

---
+++ Learning_Git.py
@@ -0,0 +1,14 @@
+def travel_time (distance, speed):
+    return distance / speed
+
+distance = int(input('Enter distance: '))
+speed = int (input('Enter speed: '))
+print(travel_time(distance,speed))
+
+distance = 100
+average_speed = 60
+
+travel_time = distance / average_speed
+
+print('The travel time is: ')
+print(travel_time)
